35b34b221f96f88683ed9934d7c7f149c2967b9b
RajitJainSinghai <noddysinghai053@gmail.com>
2023-02-13 12:25:41 +0530

    added php

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="./css/header.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<nav>
+      <div class="logo">
+         <a href=""><img id="logoImg" src="./img/logo1.png" alt="Logo Image"></a> 
+      </div>
+      <div class="hamburger">
+          <div class="bars1"></div>
+          <div class="bars2"></div>
+          <div class="bars3"></div>
+      </div>
+      <ul class="nav-links">
+          <li><a href="#">HOME</a></li>
+          <li><a href="#">ABOUT US</a></li>
+          <li><a href="./index-color.html">GALLERY</a></li>
+          <li><a href="#">CONTACT US</a></li>
+          <li class="social_icons_varsha">
+            <a id="social_links" target="_blank" href="https://www.facebook.com/mystrokz/"><img src="./img/facebook.png" alt=""></a>
+            <a id="social_links" target="_blank" href="https://www.instagram.com/Strokz_by_varsha/"><img src="./img/instagram.png" alt=""></a>
+            <a id="social_links" target="_blank" href="https://in.pinterest.com/varsha2042/?invite_code=50b22022b9d44e0ead04475c3907d568&sender=110619872005776011"><img src="./img/pinterest.png" alt=""></a>
+            <a id="social_links" target="_blank" href="https://twitter.com/varsha44892073"><img src="./img/twitter.png" alt=""></a>
+            <a id="social_links" target="_blank" href="https://www.youtube.com/@mystrokz1383"><img src="./img/youtube.png" alt=""></a>
+          </li>
+      </ul>
+  </nav>
+</body>
+</html>
--- index.php
+++ index.php
@@ -13,28 +13,8 @@
 
   </head>
   <body>
-    <nav>
-      <div class="logo">
-         <a href=""><img id="logoImg" src="./img/logo1.png" alt="Logo Image"></a> 
-      </div>
-      <div class="hamburger">
-          <div class="bars1"></div>
-          <div class="bars2"></div>
-          <div class="bars3"></div>
-      </div>
-      <ul class="nav-links">
-          <li><a href="#">HOME</a></li>
-          <li><a href="#">ABOUT US</a></li>
-          <li><a href="./index-color.html">GALLERY</a></li>
-          <li><a href="#">CONTACT US</a></li>
-          <li class="social_icons_varsha">
-            <a id="social_links" target="_blank" href="https://www.facebook.com/mystrokz/"><img src="./img/facebook.png" alt=""></a>
-            <a id="social_links" target="_blank" href="https://www.instagram.com/Strokz_by_varsha/"><img src="./img/instagram.png" alt=""></a>
-            <a id="social_links" target="_blank" href="https://in.pinterest.com/varsha2042/?invite_code=50b22022b9d44e0ead04475c3907d568&sender=110619872005776011"><img src="./img/pinterest.png" alt=""></a>
-            <a id="social_links" target="_blank" href="https://twitter.com/varsha44892073"><img src="./img/twitter.png" alt=""></a>
-          </li>
-      </ul>
-  </nav>
+  <?php include 'header.php';?>
+  
   <script src="javascript/script.js"></script>
     <section class="Art_gallery_by_varsha">
       <!-- 1 -->
